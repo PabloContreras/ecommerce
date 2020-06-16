9ecb2c267c43326b809be8974270cc2a636807b3
pablocontreras <pablo_contreras_1997@outlook.com>
2020-06-16 12:04:12 -0500

    base

---
+++ MaterialTec/MaterialTec/Controllers/Admin/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaterialTec.Controllers.Administracion
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View("../Admin/Product/Index");
+        }
+
+        public ActionResult Test()
+        {
+            return View("../Admin/Product/Test");
+        }
+    }
+}
---
+++ MaterialTec/MaterialTec/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MaterialTec.MvcApplication" Language="C#" %>
